10d207ef98afccc82e650feae00b104a93ac31f2
astrodv <98991399+astrodv@users.noreply.github.com>
2022-04-29 02:56:45 +0100

    added example.html to check the current compiled js in the dist file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { functionOne } from './functionOne';
-import { functionTwo } from './functionTwo';
-import { elseStatement } from './elseStatement';
+import { functionOne } from './functionOne.js';
+import { functionTwo } from './functionTwo.js';
+import { elseStatement } from './elseStatement.js';
 
 var num: number = 2;
 
-if ((num = 1)) {
+if ((num == 1)) {
   functionOne();
 }
-if ((num = 2)) {
+if ((num == 2)) {
   functionTwo();
 } else {
   elseStatement();
